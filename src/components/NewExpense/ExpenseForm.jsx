7975239422d4fb7c17a02b38f0b85f8832d4d19b
halilibrahimcelik <you@example.com>
2022-06-22 12:10:49 +0300

    fixing a bug.

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -81,7 +81,7 @@ return (
         </div>
         <div className='new-expense__control'>
         <label >Date</label>
-        <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+        <input type="date" value={enteredDate} min="2019-01-01" max="2024-12-31" onChange={dateChangeHandler} />
         </div>
         </div>
         <div className='new-expense__actions'>
